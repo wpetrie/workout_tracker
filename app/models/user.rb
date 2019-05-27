02db770f51521a94e621feed75902a85a0ab2964
BJ <petrie.bj@gmail.com>
2019-05-26 23:19:30 -0300

    Final before alpha

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_many :stats
+  has_many :stats
+
+
+
 end
